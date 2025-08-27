3fdf2c5ffa08ff913c1eaa7205eb307103388101
Dimitry Nazarov <dimitrynazarov@gmail.com>
2025-08-27 15:07:27 -0400

    fix: some cleanup + nvim

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,8 +1,10 @@
-local wezterm       = require 'wezterm'
-local config        = wezterm.config_builder()
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
 
-config.color_scheme = 'Gruvbox Dark (Gogh)'
-config.font         = wezterm.font 'Hack Nerd Font'
-config.font_size    = 19
+config.color_scheme = "Gruvbox Dark (Gogh)"
+config.font = wezterm.font("Hack Nerd Font")
+config.font_size = 19
+config.default_prog = { "/opt/homebrew/opt/tmux", "new-session", "-A", "-s", "main", "zsh" }
+config.automatically_reload_config = true
 
 return config
