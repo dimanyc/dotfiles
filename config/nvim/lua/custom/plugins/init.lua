7a253453c0c22161b8c84072d16b57839b3f0fa0
Dimitry Nazarov <dimitry.nazarov@meraki.net>
2025-10-06 15:26:07 -0400

    feat: adds opencode

--- config/nvim/lua/custom/plugins/init.lua
+++ config/nvim/lua/custom/plugins/init.lua
@@ -7,24 +7,34 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'github/copilot.vim',
-  },
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    dependencies = {
-      { 'nvim-lua/plenary.nvim', branch = 'master' },
-    },
-    build = 'make tiktoken',
-    keys = {
-      { '<leader>cc', '<cmd>CopilotChatOpen<cr>', desc = 'Open Copilot Chat' },
-      { '<leader>ce', ':CopilotChatExplain<cr>', mode = 'v', desc = 'Explain selected code' },
-      opts = {
-        enable_tool_calling = true,
-        -- See the configuration section below
-      },
-    },
-  },
+  -- {
+  --   'github/copilot.vim',
+  -- },
+  -- {
+  --   'CopilotC-Nvim/CopilotChat.nvim',
+  --   dependencies = {
+  --     { 'nvim-lua/plenary.nvim', branch = 'master' },
+  --   },
+  --   build = 'make tiktoken',
+  --   keys = {
+  --     { '<leader>zc', ':CopilotChat<CR>', mode = 'n', desc = 'Chat with Copilot' },
+  --     { '<leader>ze', ':CopilotChatExplain<CR>', mode = 'v', desc = 'Explain Code' },
+  --     { '<leader>zr', ':CopilotChatReview<CR>', mode = 'v', desc = 'Review Code' },
+  --     { '<leader>zf', ':CopilotChatFix<CR>', mode = 'v', desc = 'Fix Code Issues' },
+  --     { '<leader>zo', ':CopilotChatOptimize<CR>', mode = 'v', desc = 'Optimize Code' },
+  --     { '<leader>zd', ':CopilotChatDocs<CR>', mode = 'v', desc = 'Generate Docs' },
+  --     { '<leader>zt', ':CopilotChatTests<CR>', mode = 'v', desc = 'Generate Tests' },
+  --     { '<leader>zm', ':CopilotChatCommit<CR>', mode = 'n', desc = 'Generate Commit Message' },
+  --     { '<leader>zs', ':CopilotChatCommit<CR>', mode = 'v', desc = 'Generate Commit for Selection' },
+  --     opts = {
+  --       enable_tool_calling = true,
+  --       mappings = {
+  --         complete = '<C-l>',
+  --       },
+  --       -- See the configuration section below
+  --     },
+  --   },
+  -- },
   {
     'vim-test/vim-test',
     dependencies = {
